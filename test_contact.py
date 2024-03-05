375d60314a9fce387b22ba3d2e0200dbd15b451b
Sanket <telisanket2002@gmail.com>
2024-03-05 22:46:51 +0530

    comment 5

--- test_contact.py
+++ test_contact.py
@@ -1,3 +1,11 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
 def test_contact_page(client):
     response = client.get('/contact')
     assert response.status_code == 200
